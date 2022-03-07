2ef27b6b8f31e526c122ca20e9e54511bc7e1ab5
Shubham-Mistry <mistryshubham29@gmail.com>
2022-03-07 18:42:48 +0530

    Added basic content and animations.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
-const menuButton = document.getElementById('hamburger');
-const menu = document.getElementById('menu');
+const menuButton = document.querySelector('#hamburger');
+const menu = document.querySelector('#menu');
+const navs = menu.querySelectorAll('li');
+const main = document.querySelector('main');
+const sections = main.querySelectorAll('section');
+
+sections[0].classList.add('active-display');
 
 menuButton.addEventListener('click', () => {
 	menu.classList.toggle('show');
+	main.classList.toggle('blur');
+});
+
+navs.forEach((nav) => {
+	nav.addEventListener('click', () => {
+		menu.classList.remove('show');
+		main.classList.remove('blur');
+	});
+});
+
+main.addEventListener('scroll', () => {
+	const index = Math.round(main.scrollTop / main.clientHeight);
+
+	sections.forEach((section, idx) => {
+		if (idx === index) {
+			section.classList.add('active-display');
+		} else {
+			section.classList.remove('active-display');
+		}
+	});
 });
