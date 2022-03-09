d67a9d5fa0a6665adf39ba40c65554a9903336a3
Shubham-Mistry <mistryshubham29@gmail.com>
2022-03-09 15:08:55 +0530

    Added custom menu button and minor changes.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,38 @@
+const logo = document.querySelector('#logo');
 const menuButton = document.querySelector('#hamburger');
 const menu = document.querySelector('#menu');
 const navs = menu.querySelectorAll('li');
 const main = document.querySelector('main');
 const sections = main.querySelectorAll('section');
-const navigators = document.querySelectorAll('.navigator li');
+const navigator = document.querySelector('.navigator');
+const navigators = navigator.querySelectorAll('li');
 
 sections[0].classList.add('active-display');
 navigators[0].classList.add('active');
 
+// h1 Logo
+logo.addEventListener('click', () => {
+	menuButton.classList.remove('change');
+	menu.classList.remove('show');
+	main.classList.remove('blur');
+	navigator.classList.remove('blur');
+});
+
 // h1 Hamburger Icon
 menuButton.addEventListener('click', () => {
+	menuButton.classList.toggle('change');
 	menu.classList.toggle('show');
 	main.classList.toggle('blur');
+	navigator.classList.toggle('blur');
 });
 
 // h1 Menu Items
 navs.forEach((nav) => {
 	nav.addEventListener('click', () => {
+		menuButton.classList.remove('change');
 		menu.classList.remove('show');
 		main.classList.remove('blur');
+		navigator.classList.remove('blur');
 	});
 });
 
